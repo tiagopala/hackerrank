9636fc1fd6b8a78975c9312617c0db7289b5b1e8
Tiago Pala <tiago_pala@outlook.com>
2024-05-10 14:22:56 -0300

    Implemented the waves exercise using binary search approach

--- src/CodeChef/Program.cs
+++ src/CodeChef/Program.cs
@@ -6,4 +6,10 @@ TheGreatRun.Execute();
 
 CoinsAndTriangle.Execute();
 
-TheWave.Execute();
+TheWave theWave = new();
+
+theWave.ExecuteUsingArrays(); /* Took less than 3 seconds */
+
+theWave.ExecuteUsingLists(); /* Took about 3 seconds as well */
+
+theWave.ExecuteUsingHashSets(); /* Took more than 3 seconds */
